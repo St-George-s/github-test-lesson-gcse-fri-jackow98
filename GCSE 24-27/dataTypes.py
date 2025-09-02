c0b17234d37163201e0c77626968492479cb1825
Jack Walker <33574605+jackow98@users.noreply.github.com>
2025-09-02 09:24:03 +0000

    data types

---
+++ GCSE 24-27/dataTypes.py
@@ -0,0 +1,38 @@
+# Data Types
+name = "Mr Walker" #This is a string
+myAge = 45 # This is an Integer
+height = 128.5 # This is a decimal number (float/real)
+hasADriversLiscence = True # This is a boolean (True/False)
+
+# Casting (Change from one data type to another)
+age = input("Enter age: ")
+print(age)
+print(age + "10") # Concatenate two strings together (join them)
+ageAsAnInt = int(age)
+print(ageAsAnInt + 10) # Add two Integers together (maths addition)
+print("Your age is " + str(ageAsAnInt))
+
+# Data types - more examples
+stillAnInteger = -4
+myNumber = "078782282873" # Always store as a string
+
+# Arithmetic operators
+add = 10 + 10
+subtract = 10 - 10
+multiply = 10 * 10
+division = 10 / 10 # Will output a float
+integerDivision = 11 // 10 # Forces output to be an integer
+print(add, subtract, multiply, division)
+print(integerDivision)
+modulo = 2000 % 501 # Remainder of the division
+print(modulo)
+exponent = 2 ** 4 # To the power of 
+print(exponent)
+
+# Activity 1 - Take two inputs, multiply them together and output answer
+numberOne = input("Enter number : ")
+
+
+# Activity 2 - Input user's age, output age times 7
+
+# Activity 3 - Take radius as input, output volume of sphere (v = 4/3 x pi x r^3)
